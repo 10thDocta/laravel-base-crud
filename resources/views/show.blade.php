a87785cf3f410c46e090e96a1f61cbb7f5d0447f
Davide Preti <davide.preti86@gmail.com>
2020-10-28 17:21:23 +0100

    Aggiunto box di verifica per eliminare un record

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,10 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
+
         *{
             margin: 0;
             padding: 0;
             box-sizing: border-box;
+        }
+
+        body {
+            padding-top: 20px;
+        }
+
+        .btn-back {
+            margin: 20px;
+            padding: 10px 15px;
+            color: white;
+            background-color: rgb(7, 7, 136);
+            border-radius: 5px;
+            text-decoration: none;
+        }
+
+        .btn-back:hover {
+            background-color: rgb(31, 31, 184);
         }
 
         .scheda {
@@ -37,7 +55,10 @@
         }
     </style>
 </head>
+
 <body>
+
+    <a class="btn-back" href="{{route('books.index')}}">Ritorna</a>
     
     <div class="scheda">
         <h2 class="title">{{$book->title}} </h2>
@@ -46,7 +67,7 @@
         <small class="edition">Anno: {{$book->year}}</small>
         <small class="edition">Genere: {{$book->genre}}</small>
         <small class="edition">Pagine: {{$book->pages}}</small>
-        <img src="{{$book->image}} " alt="">
+        <img src="{{$book->image}} " alt="{{$book->title}} image">
     </div>
 
 
@@ -54,8 +75,9 @@
         @csrf
         @method('DELETE')
 
-        <input type="submit" value="Cancella">
+        <input type="submit" value="Cancella" onclick="return confirm('Sicuro di voler cancellare?')">
     </form>
+
 </body>
 </html>
 
